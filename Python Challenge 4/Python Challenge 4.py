062698fc23e3d93f3096b965619476c583ec2233
Leon <leonr502@gmail.com>
2020-03-04 01:40:21 +0100

    Changed Challenge 4

--- Python Challenge 4/Python Challenge 4.py
+++ Python Challenge 4/Python Challenge 4.py
@@ -1,21 +1,20 @@
-import time
-from urllib import urlopen
+from urllib.request import urlopen
+import re
 
 surl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
-si = "25357"
+si = "96791"
 fs = urlopen(surl + si)
 innertext = fs.read()
-sn = ""
-
-for n in innertext:
-	if ord(n) > 47 and ord(n) < 58:
-		sn = sn + n
+print(innertext)
+sn = str(int(innertext.split()[5]))
 
 for i in range(400):
 	fn = urlopen(surl + sn)
 	innertext = fn.read()
-	sn = ""
-	for n in innertext:
-		if ord(n) > 47 and ord(n) < 58:
-			sn = sn + n 
+	sn = str(int(innertext.split()[5]))
 	print(surl + sn)
+
+#1. Hürde bei 94485
+#2. Hürde bei 16044
+#3. Hürde bei 82682
+#peak.html
